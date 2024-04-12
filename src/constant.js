d8a8c4f96bc4b80f38401f9c6fbc45adafc8efa0
Manoj Kumar <159037969+ManojKumar1269@users.noreply.github.com>
2024-04-12 16:12:27 +0100

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://ceadar-chat-api.azurewebsites.net';
 
 export const isExpired = (access_token_expires) => {
   let newDateObj = new Date(access_token_expires) < moment(new Date()).subtract(60, 'm').toDate();
